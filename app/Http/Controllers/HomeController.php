2b2b393935b20f9a327d0be2e1c5bd77d3165a5d
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-06-16 20:25:25 +0200

    Laravel - Base - CRUD

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,9 @@ use App\Student;
 class HomeController extends Controller
 {
     public function index() {
-        // All data table
+        // ALL DATA TABLE
         //$students = Student::all();
-        // Where
+        // WHERE
         //$students = Student::where('id', 2)->get();
         //$students = Student::where('id', '!=', 2)->get();
         //$students = Student::where('name', 'Marco Dubito')->get();
@@ -18,14 +18,13 @@ class HomeController extends Controller
             ->limit(2)
             ->get();
         //$students = Student::orderBy('name', 'asc')->get();
-        // First: necessario l'inserimento in un array poichè non otteniamo una collezione di oggetti su cui ciclare ma un'unica entità
+        // FIRST: necessario l'inserimento in un array poichè non otteniamo una collezione di oggetti su cui ciclare ma un'unica entità
         //$students[] = Student::where('id', '<>', 1)->first();
-        // Find: necessario l'inserimento in un array poichè non otteniamo una collezione di oggetti su cui ciclare ma un'unica entità
+        // FIND: necessario l'inserimento in un array poichè non otteniamo una collezione di oggetti su cui ciclare ma un'unica entità
         //$students[] = Student::find(3);
         /*$students = Student::all();
         $student = $students->find(3);
         dd($student->name);*/
-        // Find
         return view('home', compact('students'));
     }
 }
